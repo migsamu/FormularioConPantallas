96130e97a804033c2f92b237135ece1fe48a1cea
Miguel <mufrikiler@gmail.com>
2021-11-04 01:54:47 +0100

    faltan validaciones y fix nombre pantalla 2

--- app/src/main/java/org/iesfm/formularioconpantallas/MainActivity.java
+++ app/src/main/java/org/iesfm/formularioconpantallas/MainActivity.java
@@ -2,13 +2,58 @@ package org.iesfm.formularioconpantallas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static final String FORMULARIO_CON_PANTALLAS_NAME = "org.iesfm.formularioconpantallas.name";
+    protected static final String FORMULARIO_CON_PANTALLAS_SURNAME = "org.iesfm.formularioconpantallas.surname";
+    protected static final String FORMULARIO_CON_PANTALLAS_AGE = "org.iesfm.formularioconpantallas.age";
+    protected static final String FORMULARIO_CON_PANTALLAS_CARNET = "org.iesfm.formularioconpantallas.carnet";
+
+    private EditText etName;
+    private EditText etSurname;
+    private EditText etAge;
+    private CheckBox cbCarnet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.etNombre);
+        etSurname = (EditText) findViewById(R.id.etApellidos);
+        etAge = (EditText) findViewById(R.id.etEdead);
+        cbCarnet = (CheckBox) findViewById(R.id.cbCarnet);
+
+        cbCarnet.setChecked(false);
+    }
+
+    public void enviarInformacion(View v) {
+
+        Intent pantallaSecundaria = new Intent(this, MainActivity2.class);
+
+        String name = etName.getText().toString();
+        String surname = etSurname.getText().toString();
+        String age = etAge.getText().toString();
+        String carnet;
+
+        if (cbCarnet.isChecked()) {
+            carnet = "Si tengo carnet";
+        } else {
+            carnet = "No tengo carnet";
+        }
+
+        pantallaSecundaria.putExtra(FORMULARIO_CON_PANTALLAS_NAME, name);
+        pantallaSecundaria.putExtra(FORMULARIO_CON_PANTALLAS_SURNAME, surname);
+        pantallaSecundaria.putExtra(FORMULARIO_CON_PANTALLAS_AGE, age);
+        pantallaSecundaria.putExtra(FORMULARIO_CON_PANTALLAS_AGE, carnet);
+
+
+        startActivity(pantallaSecundaria);
     }
 }
--- app/src/main/java/org/iesfm/formularioconpantallas/MainActivity2.java
+++ app/src/main/java/org/iesfm/formularioconpantallas/MainActivity2.java
@@ -2,13 +2,50 @@ package org.iesfm.formularioconpantallas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.security.PublicKey;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private TextView tvName;
+    private TextView tvSurname;
+    private TextView tvAge;
+    private TextView tvCarnet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        inicializarValores();
+
+        Intent pantallaPrincipal = getIntent();
+
+        String name = pantallaPrincipal.getStringExtra(MainActivity.FORMULARIO_CON_PANTALLAS_NAME);
+        String surname = pantallaPrincipal.getStringExtra(MainActivity.FORMULARIO_CON_PANTALLAS_SURNAME);
+        String age = pantallaPrincipal.getStringExtra(MainActivity.FORMULARIO_CON_PANTALLAS_AGE);
+        String carnet = pantallaPrincipal.getStringExtra(MainActivity.FORMULARIO_CON_PANTALLAS_CARNET);
+
+        tvName.setText(name);
+        tvSurname.setText(surname);
+        tvAge.setText(age);
+        tvCarnet.setText(carnet);
+    }
+
+    public void inicializarValores() {
+        tvName = (TextView) findViewById(R.id.tvNombreIntroducido);
+        tvSurname = (TextView) findViewById(R.id.tvApellidoIntroducido);
+        tvAge = (TextView) findViewById(R.id.tvEdadIntroducida);
+        tvCarnet = (TextView) findViewById(R.id.tvCarnetIntroducido);
+    }
+
+    public void volver(View v) {
+        Intent pantallaPrincipal = new Intent(this, MainActivity.class);
+        startActivity(pantallaPrincipal);
     }
 }
